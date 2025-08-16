f7dc872e9f598651fff664f4b7c7a8f78ad9ec3f
hebizo <tabekko0926@gmail.com>
2025-08-17 00:38:45 +0900

    feature: bootstrapを導入

--- app/javascript/entrypoints/application.jsx
+++ app/javascript/entrypoints/application.jsx
@@ -1,6 +1,8 @@
 // paper_db/app/javascript/entrypoints/application.jsx
 
 import React from 'react';
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App';
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+
+export default defineConfig({
+  plugins: [
+    RubyPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/app/javascript'
+    }
+  },
+  optimizeDeps: {
+    include: ['bootstrap']
+  }
+})
